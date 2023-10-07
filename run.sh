292148fa2ca8706e094d3fc0f17233219e4ecbfa
Benjamin Ho <bhgm93@gmail.com>
2023-10-07 14:00:03 +0800

    add data analysis notebook

---
+++ run.sh
@@ -0,0 +1,2 @@
+python clean_data.py --input_file data/train.csv --output_file data/train_cleaned.csv
+python preprocess_data.py --input_file data/train_cleaned.csv --output_file data/train_preprocessed.csv

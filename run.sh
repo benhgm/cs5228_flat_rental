7daa9321cb7644db2d8416bccf44cad7fe6088a0
Benjamin Ho <bhgm93@gmail.com>
2023-10-09 19:38:26 +0800

    Revert "add data analysis notebook"
    This reverts commit 292148fa2ca8706e094d3fc0f17233219e4ecbfa.

--- run.sh
+++
@@ -1,2 +0,0 @@
-python clean_data.py --input_file data/train.csv --output_file data/train_cleaned.csv
-python preprocess_data.py --input_file data/train_cleaned.csv --output_file data/train_preprocessed.csv
